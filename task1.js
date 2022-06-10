afe59f0048e5ab61b775f0a53dadfcd2aaf16e28
Amir-Aminian <amir_aminian@hotmail.com>
2022-06-09 23:11:52 -0230

    task1

---
+++ task1.js
@@ -0,0 +1,14 @@
+
+let task1 = [1, -1, 0, -1, -1, -1, -1];
+let count = 1;
+calculate_count(...task1);
+function calculate_count() {
+    for (let i = 0; i < (task1.length-1); i++) {
+        if (task1[i] == task1[i+1]) {
+            count = count + 1;
+        } else {
+            count = 1;
+        }
+    }
+    alert("count: " + count);
+}
