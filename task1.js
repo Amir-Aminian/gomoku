7ff1d12b81081d767e4928af882bc9d54e13e98e
Amir-Aminian <amir_aminian@hotmail.com>
2022-06-10 20:10:14 -0230

    fixed the problems

--- task1.js
+++ task1.js
@@ -1,13 +1,18 @@
-let task1 = [1, -1, 0, -1, -1, -1, -1];
+let task1 = [1, -1, 0, -1, 0, -1, -1, 0];
+let max_count = 0;
 let count = 1;
 calculate_count(...task1);
 function calculate_count() {
     for (let i = 0; i < (task1.length-1); i++) {
         if (task1[i] == task1[i+1]) {
             count = count + 1;
+            if (count > max_count) {
+                max_count = count
+            }
+            max_count = count
         } else {
             count = 1;
         }
     }
-    alert("count: " + count);
+    alert("count: " + max_count);
 }
