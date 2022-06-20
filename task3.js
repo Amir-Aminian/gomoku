109a97c0f72bee5ee6e5573474771e19b1496d54
Amir-Aminian <amir_aminian@hotmail.com>
2022-06-20 20:16:40 -0230

    fixed the id problem

--- task3.js
+++ task3.js
@@ -8,15 +8,28 @@ const layers = ["background", "chips"];
 for (let i = 0; i < layers.length; i++) {
     const table = document.createElement("table");
     table.id = layers[i];
-    for (let i = 0; i < 15; i++) {
-        const row = document.createElement("tr");
-        row.id = i;
+    if (table.id == "chips") {
         for (let i = 0; i < 15; i++) {
-            const column = document.createElement("td");
-            column.id = row.id + "_" + i;
-            row.appendChild(column);
+            const row = document.createElement("tr");
+            row.id = i;
+            for (let i = 0; i < 15; i++) {
+                const column = document.createElement("td");
+                column.id = row.id + "_" + i;
+                row.appendChild(column);
+            }
+            table.appendChild(row);
         }
-        table.appendChild(row);
+    } else {
+        if (table.id == "background") {
+            for (let i = 0; i < 14; i++) {
+                const row = document.createElement("tr");
+                for (let i = 0; i < 14; i++) {
+                    const column = document.createElement("td");
+                    row.appendChild(column);
+                }
+                table.appendChild(row);
+            }
+        }
     }
     gameboard.appendChild(table);
 }
