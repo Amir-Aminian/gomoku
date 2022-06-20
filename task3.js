7b07eb8a389094f6f018e22aaa2e675047a7d2cf
Amir-Aminian <amir_aminian@hotmail.com>
2022-06-20 19:47:32 -0230

    added comments to task3

--- task3.js
+++ task3.js
@@ -1,7 +1,9 @@
 // creats the game enviroment
+// creats a div with id of gameboard and appends it to the body
 const gameboard = document.createElement("div");
 gameboard.id = "gameboard";
 document.body.appendChild(gameboard);
+// creats two table on top of eachother as background and chips with ids of background and chips and appends them to the gameboard div
 const layers = ["background", "chips"];
 for (let i = 0; i < layers.length; i++) {
     const table = document.createElement("table");
