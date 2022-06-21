b2f69fa990c49b926d52fa967f8d143299210e08
Amir-Aminian <amir_aminian@hotmail.com>
2022-06-21 21:03:59 -0230

    added parameter to the function

--- task5.js
+++ task5.js
@@ -1,6 +1,5 @@
 // data model
-function dataModel() {
-    let dataModel = [];
+function dataModel(dataModel) {
     for (let rowId = 0; rowId < 15; rowId++) {
         for (let columnId = 0; columnId < 15; columnId++) {
             column.innerHTML = (dataModel[rowId][columnId]);
