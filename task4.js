388977c8143dbdcc6966372481de181ea046e798
Amir-Aminian <amir_aminian@hotmail.com>
2022-06-21 16:42:51 -0230

    fixed task4 problem

--- task4.js
+++ task4.js
@@ -2,9 +2,9 @@
 function dataModel() {
     let dataModel = [];
     for (let rowId = 0; rowId < 15; rowId++) {
+        let rowArray = [];
         for (let columnId = 0; columnId < 15; columnId++) {
-            rowArray = [];
-            rowArray.push(document.getElementById(rowId + "_" + columnId));
+            rowArray.push(document.getElementById(rowId + "_" + columnId).innerHTML);
         }
         dataModel.push(rowArray);
     }
