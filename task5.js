1f1d4aa032940fab936de4c561ac7654413346fa
Amir-Aminian <amir_aminian@hotmail.com>
2022-07-14 19:06:30 -0230

    did some clean up

--- task5.js
+++
@@ -1,7 +0,0 @@
-const sendDataModel = (dataModel) => {
-    for (let rowId = 0; rowId < dataModel.lenght; rowId++) {
-        for (let columnId = 0; columnId < dataModel.lenght; columnId++) {
-            document.getElementById(rowId + "_" + columnId).innerHTML = (dataModel[rowId][columnId]);
-        }
-    }
-}
--- task6.js
+++
@@ -1,8 +0,0 @@
-const updateDataModel = (dataModel, rowId, columnId, player) => {
-    if (player == 0) {
-        dataModel[rowId][columnId] = 2;
-    } else {
-    dataModel[rowId][columnId] = 1;
-    }
-    return dataModel;
-}
